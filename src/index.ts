346fcde40e36ccb8b32d6b7569e3b4186bcc3728
steelydylan <ess_president@me.com>
2020-06-25 16:25:30 +0900

    push

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ function doPost(e) {
   const json = JSON.parse(e.parameters.payload.toString())
   if (json.type == "block_actions") {
     const responseUrl = json.response_url
-    const people = json.actions[0].value
+    const { people, title, start, end, description } = JSON.parse(json.actions[0].value)
     const text = {"text" : '*:man-lifting-weights: `' + json.message.blocks[0].text.text + '` に決まりました :man-lifting-weights:*\n' + people} 
     const payload = JSON.stringify(text)
     const options = {
@@ -13,44 +13,36 @@ function doPost(e) {
         "contentType": "application/json",
         "payload": payload
     };
-    registerSchedule({ people, date: json.message.blocks[0].text.text, title: 'test' });
+    registerSchedule({ people, start, end, title, description });
     UrlFetchApp.fetch(responseUrl, options);
   }
 }
-      
-function postToSlack() {
-  const people = '<@U010SEMAGHH>' // @Naoto Tanaka
-  const message = createHeadMessage(people)
-  const title = '打ち合わせ'
-  const startTime = new Date().toString();
-  const endTime = new Date().toString();
-  const description = '打ち合わせURLはこちらです。zoomURL: https://zoom.us/**/**';
 
-  const suggest1 = createSuggest("11/03(水) 11:00 ~ 12:00", {
-    title,
-    people,
-    startTime,
-    endTime,
-    description,
-  })
-  const suggest2 = createSuggest("11/04(木) 12:00 ~ 13:00", {
-    title,
-    people,
-    startTime,
-    endTime,
-    description,
-  })
-  const suggest3 = createSuggest("11/05(金) 15:00 ~ 16:00", {
-    title,
-    people,
-    startTime,
-    endTime,
-    description,
-  })
-  post(message)
-  post(suggest1)
-  post(suggest2)
-  post(suggest3)
+type Event = {
+  start: string;
+  end: string;
+}
+
+type Props = {
+  title: string;
+  people: string;
+  events: Event[];
+  description: string;
+}
+      
+function postToSlack({ title, people, events, description }: Props) {
+  var message = createHeadMessage(people);
+  post(message);
+  events.forEach(({start, end}) => {
+    const suggest = createSuggest(`${start} ~ ${end}`, {
+      title,
+      people,
+      start,
+      end,
+      description
+    });
+    post(suggest);
+  });
 }
 
 /**
@@ -93,7 +85,7 @@ function createSuggest(text, value) {
             "text": "この日に決める"
           },
           "style": "primary",
-          "value": value,
+          "value": JSON.stringify(value),
           "action_id": "button"
         }
       }
@@ -125,11 +117,17 @@ function createEvents({ title, id, start, end, description }) {
   calendar.createEvent(title, startTime, endTime, option);
 }
 
-function registerSchedule({ people, date, title } : { people: string, date: string, title: string }) {
+function registerSchedule({ people, start, end, title, description } : { people: string, start: string, end: string, title: string, description: string }) {
   const persons = people.split(',');
   persons.forEach((person) => {
     const gmail = convertSlackIDtoGmail(person);
-    createEvents
+    createEvents({
+      id: gmail,
+      start,
+      end,
+      title,
+      description,
+    })
   })
 }
 
@@ -147,6 +145,9 @@ function convertSlackIDtoGmail(slackID: string) {
 }
 
 function test() {
-  const gmail = convertSlackIDtoGmail('<@U010U6744NN>');
-  createEvents({ id: gmail, title: 'テスト', description: 'ほげほげ', start: '2020/06/26 15:00', end: '2020/06/26 16:00' });
+  const people = '<@U010U6744NN>'; // @Naoto Tanaka
+  const title = '打ち合わせ';
+  const description = '打ち合わせURLはこちらです。zoomURL: https://zoom.us/**/**';
+  const events = [{ start: "2020/06/26 15:00", end: '2020/06/26 16:00' }, { start: "2020/06/27 15:00", end: '2020/06/27 16:00' }]
+  postToSlack({ title, description, people, events });
 }
